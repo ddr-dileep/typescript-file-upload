6f922bc9d7d64a3efc5b24ed24bfe579db4e7593
Dileep <ddr.dileepraloti@gmail.com>
2024-01-08 17:44:33 +0530

    remove dist

--- dist/utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (error, _req, res, _next) => {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-};
-exports.errorHandler = errorHandler;
